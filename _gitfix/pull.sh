a4729964de20f573a0a0971d58cd7794902d38f0
懒懒的风 <220881428+3311930677@users.noreply.github.com>
2026-09-19 18:17:26 +0800

    chore: 移除修推送遗留的 _gitfix 临时目录（pull.sh / want_objects.txt）

--- _gitfix/pull.sh
+++
@@ -1,5 +0,0 @@
-cd "D:/new bee"
-git -c http.proxy=http://127.0.0.1:63133 \
-    -c http.version=HTTP/1.1 \
-    -c http.postBuffer=524288000 \
-    fetch-pack --stdin --all https://github.com/3311930677/new-bee.git < _gitfix/want_objects.txt
